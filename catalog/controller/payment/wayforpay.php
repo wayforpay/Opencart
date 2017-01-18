2ff0c4c222141fd946ac24656ed184a951dab7c2
asikk <asikk@users.noreply.github.com>
2017-01-18 09:43:08 +0200

    Update wayforpay.php
    add php old version support

--- catalog/controller/payment/wayforpay.php
+++ catalog/controller/payment/wayforpay.php
@@ -48,7 +48,7 @@ class ControllerPaymentWayforpay extends Controller
         $this->load->model('account/order');
         $products = $this->model_account_order->getOrderProducts($order_id);
         foreach ($products as $product) {
-            $productNames[] = str_replace(["'", '"', '&#39;', '&'], '', htmlspecialchars_decode($product['name']));
+            $productNames[] = str_replace(array("'", '"', '&#39;', '&'), '', htmlspecialchars_decode($product['name']));
             $productPrices[] = round($product['price']);
             $productQty[] = $product['quantity'];
         }
